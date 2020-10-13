e8044821b41075c114cf5a87ee00affea2a8f4f0
dominik <dominik@ntb>
2020-10-13 15:15:43 +0200

    Introduce types for the store

---
+++ src/store/types.ts
@@ -0,0 +1,31 @@
+export type TodoItem = {
+    id: string,
+    text: string,
+    checked: boolean,
+    deleted: boolean
+}
+
+export type TodoListState = {
+    items: Array<TodoItem>
+}
+
+export const ADD_ITEM = 'ADD_ITEM'
+export const TOGGLE_ITEM = 'TOGGLE_ITEM'
+export const DELETE_ITEM = 'DELETE_ITEM'
+
+export type AddItemAction = {
+    type: typeof ADD_ITEM,
+    payload: string
+}
+
+export type ToggleItemAction ={
+    type: typeof TOGGLE_ITEM,
+    payload: string
+}
+
+export type DeleteItemAction = {
+    type: typeof DELETE_ITEM,
+    payload: string
+}
+
+export type TodoActionTypes = AddItemAction | ToggleItemAction | DeleteItemAction
